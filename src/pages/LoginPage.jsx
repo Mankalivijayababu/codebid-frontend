11c1dc6eb194e1c4d8230396932b9296a9340e7d
Your GitHub Username <Mankalivijayababu>
2026-02-19 19:44:34 +0530

    Production API env update

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 /* 🔥 PRODUCTION API CONFIG */
 const BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000";
+  import.meta.env.VITE_API_URL ;
 
 const API = `${BASE_URL}/api`;
 
--- src/pages/TeamDashboard.jsx
+++ src/pages/TeamDashboard.jsx
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 
 /* 🔥 PRODUCTION SAFE ENV CONFIG */
 const BASE_URL =
-  import.meta.env.VITE_API_URL ?? "http://localhost:5000";
+  import.meta.env.VITE_API_URL ;
 
 const API = `${BASE_URL}/api`;
 const SOCKET_URL = BASE_URL;
